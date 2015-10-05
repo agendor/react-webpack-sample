f858b3a96e0bf27db0ca5da1aa713ccede3f5469
Tulio Monte Azul <tuliomonteazul@gmail.com>
2015-10-05 01:43:21 -0300

    Change app.jsx to render component

--- src/app.jsx
+++ src/app.jsx
@@ -6,8 +6,16 @@ var App = React.createClass({
   render: function () {
     return (
       <div>
-        App main component
+        This is a React component!
       </div>
     );
   }
 });
+
+
+module.exports.App = App;
+
+if (typeof document != undefined) {
+  var container = document.getElementById("react-container");
+  React.render(React.createElement(App), container);
+}
